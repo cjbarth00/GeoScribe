7bffa41d0172281aa288a1466cb48edec299a489
Casey Barth <cjbarth1@gmail.com>
2023-02-27 15:53:11 -0800

    Initial Commit

---
+++ GeoScribe/GeoScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoScribeApp.swift
+//  GeoScribe
+//
+//  Created by Casey Barth on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
